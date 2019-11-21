66b07e442edf2d48d5ba6f200657e652b36ebca6
Ferran Gurri Mancera <ferran.gurri@gmail.com>
2019-11-21 22:19:56 +0100

    Add files via upload

---
+++ src/binomi/game/cluedo/GameCluedo.java
@@ -0,0 +1,15 @@
+package binomi.game.cluedo;
+
+import java.util.ArrayList;
+
+import binomi.game.Board;
+import binomi.game.Game;
+import binomi.game.Player;
+
+public class GameCluedo extends Game {
+
+	public GameCluedo(String name, Board board, ArrayList<Player> players) {
+		super(name, board, players);
+	}
+
+}
